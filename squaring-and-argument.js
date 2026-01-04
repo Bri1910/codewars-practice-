7a7352aa60ee7ea07b7e852f66360f1e2b36d2bb
Bri1910 <bdsb_1910@outlook.com>
2026-01-04 09:58:12 -0500

    Add new Codewars challenges

---
+++ squaring-and-argument.js
@@ -0,0 +1,12 @@
+//Description:
+//Now you have to write a function that takes an argument
+//  and returns the square of it.
+function square(num){
+return num * num ;// Write the "square"-function here
+}
+//
+const square = (n) => n * n;
+//
+function square(x) {
+  return x ** 2;
+}
