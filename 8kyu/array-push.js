06bc6dfa63fe6a13e3557b749792d4402f5a8fdc
Bri1910 <bdsb_1910@outlook.com>
2026-02-23 19:00:11 -0500

    Add 8kyu arry push challenge

---
+++ 8kyu/array-push.js
@@ -0,0 +1,5 @@
+//Add the value ''codewars'' to the website array.
+//after your code executes the website array shoudl ==['codewars']
+//the website array has already been defined fo your using the folloeing code
+let website = [];
+website.push('codewars');
